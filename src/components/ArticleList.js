ad8c7f875a95102f2093d41063161fe4ece3dfdb
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-08-04 21:52:06 +0300

    lab complete

---
+++ src/components/ArticleList.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList() {
+  const posts = [
+    {
+      id: 1,
+      title: "Components 101",
+      date: "December 15, 2020",
+      preview: "Setting up the building blocks of your site",
+      minutes: 5,
+    },
+    {
+      id: 2,
+      title: "React Data Flow",
+      date: "December 11, 2020",
+      preview: "Passing props is never passé",
+      minutes: 15,
+    },
+    {
+      id: 3,
+      title: "Function vs Class Components",
+      preview: "React, meet OOJS.",
+      minutes: 47,
+    },
+  ];
+
+  return (
+    <main>
+      {posts.map((post) => (
+        <Article 
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+        />
+      ))}
+    </main>
+  );
+}
+
+export default ArticleList;
